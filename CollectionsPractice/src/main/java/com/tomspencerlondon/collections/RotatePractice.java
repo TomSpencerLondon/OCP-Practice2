8adc9393902bceb66b831fc90a653d00ef0211f8
Tom Spencer <thomas.spencer@codurance.com>
2022-12-16 13:17:16 +0000

    Edit RotatePractice

--- CollectionsPractice/src/main/java/com/tomspencerlondon/collections/RotatePractice.java
+++ CollectionsPractice/src/main/java/com/tomspencerlondon/collections/RotatePractice.java
@@ -50,12 +50,4 @@ public class RotatePractice {
     }
   }
 
-  public static void swap(List<?> list, int i, int j) {
-    // instead of using a raw type here, it's possible to capture
-    // the wildcard but it will require a call to a supplementary
-    // private method
-    final List l = list;
-    l.set(i, l.set(j, l.get(i)));
-  }
-
 }
