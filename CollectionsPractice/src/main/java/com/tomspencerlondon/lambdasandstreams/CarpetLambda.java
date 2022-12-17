c5f2addfce7c7a5bd4eed2da4ace80f50d462b44
Tom Spencer <thomas.spencer@codurance.com>
2022-12-17 13:37:48 +0000

    Add LambdaPractice

---
+++ CollectionsPractice/src/main/java/com/tomspencerlondon/lambdasandstreams/CarpetLambda.java
@@ -0,0 +1,34 @@
+package com.tomspencerlondon.lambdasandstreams;
+
+public class CarpetLambda {
+
+  interface Carpet {
+    double calcArea(double l, double w);
+  }
+
+  interface CalcPrice {
+    double calc(double area);
+  }
+
+  interface CalcTotal {
+    double calc(double cp, double lp);
+  }
+
+  public static void main(String[] args) {
+    Carpet area = (l, w) -> l * w;
+    CalcPrice lp = (sqm) -> 10 * sqm;
+    CalcPrice cp = (sqm) -> 15 * sqm;
+    CalcTotal totalPrice = (l, c) -> l + c;
+
+    double calcArea = area.calcArea(2.0, 4.0);
+    double labourcost = lp.calc(calcArea);
+    double carpetCost = cp.calc(calcArea);
+    double total = totalPrice.calc(labourcost, carpetCost);
+
+    System.out.println("Area: " + calcArea);
+    System.out.println("Labour cost: " + labourcost);
+    System.out.println("Carpet cost: " + carpetCost);
+
+    System.out.println("Final cost: " + total);
+  }
+}
--- CollectionsPractice/src/main/java/com/tomspencerlondon/lambdasandstreams/LambdaPractice.java
+++ CollectionsPractice/src/main/java/com/tomspencerlondon/lambdasandstreams/LambdaPractice.java
@@ -3,15 +3,17 @@ package com.tomspencerlondon.lambdasandstreams;
 public class LambdaPractice {
 
   interface MovieCalc {
-    double movieprice(int x, int y);
+    double movieprice(double x, double y);
   }
 
   public static void main(String[] args) {
     MovieCalc obj = (x, y) -> x * y;
     MovieCalc obj2 = (x, y) -> x + y;
+    double tp = obj2.movieprice(5, 2);
     MovieCalc obj3 = (x, y) -> x / y;
 
-    double tp = obj.movieprice(5, 2);
+    double other = obj3.movieprice(5, 2);
     System.out.println(tp);
+    System.out.println(other);
   }
 }
