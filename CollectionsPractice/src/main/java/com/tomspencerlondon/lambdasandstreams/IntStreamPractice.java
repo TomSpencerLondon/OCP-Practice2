13a14acf12ba9bcf7023b7e069aec574860e0b5d
Tom Spencer <thomas.spencer@codurance.com>
2022-12-21 18:43:10 +0000

    Random ints array

--- CollectionsPractice/src/main/java/com/tomspencerlondon/lambdasandstreams/IntStreamPractice.java
+++ CollectionsPractice/src/main/java/com/tomspencerlondon/lambdasandstreams/IntStreamPractice.java
@@ -1,5 +1,6 @@
 package com.tomspencerlondon.lambdasandstreams;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -14,6 +15,10 @@ public class IntStreamPractice {
     System.out.println("Sum: ----");
     System.out.println(stream.summaryStatistics());
 //    System.out.println(stream.sum());
+
+    int[] ints = new Random().ints(52, 1, 10).toArray();
+
+    System.out.println(Arrays.toString(ints));
 
   }
 }
