847ee5e94dd99c2bc37ae8cd356ef4446fbd26ce
Tom Spencer <thomas.spencer@codurance.com>
2023-01-07 12:16:07 +0000

    Add exception and file reading

--- Exceptions-CSV/src/main/java/com/tomspencerlondon/FileDelimitedCsv.java
+++ Exceptions-CSV/src/main/java/com/tomspencerlondon/FileDelimitedCsv.java
@@ -1,10 +1,40 @@
 package com.tomspencerlondon;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class FileDelimitedCsv {
 
-  public static void main(String[] args) {
-//    URL resource = getClass().getClassLoader().getResource("file.txt");
+  public static void main(String[] args) throws IOException {
+    String filename = "src/main/resources/DataTradeClients.csv";
+    String line;
+    int count = 0;
+    boolean b = true;
+    Scanner inputFile = null;
+
+    try {
+      File file = new File(filename);
+      inputFile = new Scanner(file);
+      inputFile.useDelimiter("\n");
+    } catch (Exception e) {
+      System.out.println("File error " + e);
+      b = false;
+    }
+
+    while (inputFile.hasNext() && b == true) {
+      line = inputFile.next();
+      System.out.println(line);
+      count++;
+    }
+
+    if (b) { inputFile.close(); }
+    System.out.println("Record Count on File " + count);
   }
 }
--- Exceptions-CSV/src/main/java/com/tomspencerlondon/exceptions/Exceptions.java
+++ Exceptions-CSV/src/main/java/com/tomspencerlondon/exceptions/Exceptions.java
@@ -1,20 +1,31 @@
 package com.tomspencerlondon.exceptions;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Exceptions {
 
   public static void main(String[] args) {
     int smaller = 0;
     int larger = 10;
     int result = 0;
+    Scanner s = new Scanner(System.in);
 
     try {
+      System.out.println("Enter a number ");
+      smaller = s.nextInt();
       result = larger /smaller;
       System.out.println("Line 13 ");
+    } catch(ArithmeticException e) {
+      System.out.println("Arithmetic found " + e);
+    } catch(InputMismatchException e) {
+      System.out.println("InputMismatchException " + e);
     } catch (Exception e) {
       System.out.println("Error found " + e);
     }
 
     System.out.println("the end");
+    s.close();
   }
 
 }
