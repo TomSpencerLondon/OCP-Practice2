345d838bfad9f0b4b25eba19c6be620a9aba552f
Tom Spencer <thomas.spencer@codurance.com>
2023-01-07 11:59:08 +0000

    Add csv and files

---
+++ Exceptions-CSV/src/main/java/com/tomspencerlondon/FileDelimitedCsv.java
@@ -0,0 +1,10 @@
+package com.tomspencerlondon;
+
+import java.net.URL;
+
+public class FileDelimitedCsv {
+
+  public static void main(String[] args) {
+//    URL resource = getClass().getClassLoader().getResource("file.txt");
+  }
+}
---
+++ Exceptions-CSV/src/main/java/com/tomspencerlondon/exceptions/Exceptions.java
@@ -0,0 +1,20 @@
+package com.tomspencerlondon.exceptions;
+
+public class Exceptions {
+
+  public static void main(String[] args) {
+    int smaller = 0;
+    int larger = 10;
+    int result = 0;
+
+    try {
+      result = larger /smaller;
+      System.out.println("Line 13 ");
+    } catch (Exception e) {
+      System.out.println("Error found");
+    }
+
+    System.out.println("the end");
+  }
+
+}
